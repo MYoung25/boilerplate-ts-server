0d5b6386e267f2e4672f95ec0fe0c45ad27f1991
Michael Young <michael25young25@gmail.com>
2022-04-13 14:35:56 -0400

    healthcheck 503 test

--- src/routes/index.test.ts
+++ src/routes/index.test.ts
@@ -25,4 +25,10 @@ describe('/ping', () => {
         expect(response.status).toBe(200)
     })
 
+    it('should return 503 if the database is disconnected', async () => {
+        await connection.disconnect()
+        const response = await request(app).get('/ping')
+        expect(response.status).toBe(503)
+    })
+
 })
