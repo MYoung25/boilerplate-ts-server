3bd81e112a153ffb6549fb692288982f11a80869
Michael Young <michael25young25@gmail.com>
2022-04-18 15:30:31 -0400

    finishing refactor for /user to /users

--- src/routes/Users.test.ts
+++ src/routes/Users.test.ts
@@ -12,7 +12,7 @@ declare global {
 const mockConsoleError = jest.spyOn(console, 'error')
     .mockImplementation((err: ErrnoException) => {})
 
-describe('/api/User', () => {
+describe('/api/Users', () => {
     let connection: any
     beforeAll(async () => {
         connection = await mongoose.connect(global.__MONGO_URI__ as string)
@@ -33,12 +33,12 @@ describe('/api/User', () => {
         })
 
         it('returns a 200', async () => {
-            const response = await request(app).get('/User')
+            const response = await request(app).get('/Users')
             expect(response.statusCode).toBe(200)
         })
 
         it('returns all Users', async() => {
-            const response = await request(app).get('/User')
+            const response = await request(app).get('/Users')
             expect(response.body.length).toBe(1)
         })
 
@@ -51,17 +51,17 @@ describe('/api/User', () => {
         })
 
         it('returns a 201', async () => {
-            const response = await request(app).post('/User').send({})
+            const response = await request(app).post('/Users').send({})
             expect(response.statusCode).toBe(201)
         })
 
         it('returns the new User', async () => {
-            const response = await request(app).post('/User').send({ firstName: 'User' })
+            const response = await request(app).post('/Users').send({ firstName: 'User' })
             expect(response.body).toHaveProperty('firstName', 'User')
         })
 
         it('inserts the new User', async () => {
-            const response = await request(app).post('/User').send({ firstName: 'User' })
+            const response = await request(app).post('/Users').send({ firstName: 'User' })
             const item = await User.findById(response.body._id)
             expect(item).toHaveProperty('firstName', 'User')
         })
@@ -82,23 +82,23 @@ describe('/api/User', () => {
         describe('GET', () => {
 
             it('returns a 200', async () => {
-                const response = await request(app).get(`/User/${item._id}`)
+                const response = await request(app).get(`/Users/${item._id}`)
                 expect(response.statusCode).toBe(200)
             })
 
             it('returns the User', async () => {
-                const response = await request(app).get(`/User/${item._id}`)
+                const response = await request(app).get(`/Users/${item._id}`)
                 expect(response.body).toHaveProperty('_id', expect.any(String))
                 expect(response.body).toHaveProperty('__v', 0)
             })
 
             it('returns a 404', async () => {
-                const response = await request(app).get(`/User/${new mongoose.Types.ObjectId()}`)
+                const response = await request(app).get(`/Users/${new mongoose.Types.ObjectId()}`)
                 expect(response.statusCode).toBe(404)
             })
 
             it('sends a 500 if a cast error occurs on _id', async () => {
-                const response = await request(app).get(`/User/dfghjkkjhgf`)
+                const response = await request(app).get(`/Users/dfghjkkjhgf`)
                 expect(response.statusCode).toBe(500)
             })
 
@@ -107,22 +107,22 @@ describe('/api/User', () => {
         describe('PATCH', () => {
 
             it('returns a 200', async () => {
-                const response = await request(app).patch(`/User/${item._id}`).send({ firstName: 'User' })
+                const response = await request(app).patch(`/Users/${item._id}`).send({ firstName: 'User' })
                 expect(response.statusCode).toBe(200)
             })
 
             it('returns the updated User', async () => {
-                const response = await request(app).patch(`/User/${item._id}`).send({ firstName: 'Superman' })
+                const response = await request(app).patch(`/Users/${item._id}`).send({ firstName: 'Superman' })
                 expect(response.body).toHaveProperty('firstName', 'Superman')
             })
 
             it('returns a 404', async () => {
-                const response = await request(app).patch(`/User/${new mongoose.Types.ObjectId()}`).send({ name: 'User' })
+                const response = await request(app).patch(`/Users/${new mongoose.Types.ObjectId()}`).send({ name: 'User' })
                 expect(response.statusCode).toBe(404)
             })
 
             it('sends a 500 if a cast error occurs on _id', async () => {
-                const response = await request(app).patch(`/User/dfghjkkjhgf`).send({ name: 'User' })
+                const response = await request(app).patch(`/Users/dfghjkkjhgf`).send({ name: 'User' })
                 expect(response.statusCode).toBe(500)
             })
 
@@ -131,23 +131,23 @@ describe('/api/User', () => {
         describe('DELETE', () => {
 
             it('returns a 204', async () => {
-                const response = await request(app).delete(`/User/${item._id}`)
+                const response = await request(app).delete(`/Users/${item._id}`)
                 expect(response.statusCode).toBe(204)
             })
 
             it('deletes the User', async () => {
-                await request(app).delete(`/User/${item._id}`)
+                await request(app).delete(`/Users/${item._id}`)
                 const found = await User.findById(item._id)
                 expect(found).toBeNull()
             })
 
             it('tries to delete a nonexistent User', async () => {
-                const response = await request(app).delete(`/User/${new mongoose.Types.ObjectId()}`)
+                const response = await request(app).delete(`/Users/${new mongoose.Types.ObjectId()}`)
                 expect(response.statusCode).toBe(404)
             })
 
             it('sends a 500 if an error occurs', async () => {
-                const response = await request(app).delete(`/User/dfghjkkjhgf`)
+                const response = await request(app).delete(`/Users/dfghjkkjhgf`)
                 expect(response.statusCode).toBe(500)
             })
 
