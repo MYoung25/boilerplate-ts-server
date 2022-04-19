4f4556f32783774ea7473a278656129bcf693a0b
Michael Young <michael25young25@gmail.com>
2022-04-19 11:10:07 -0400

    add permissions seeding to entrypoint command

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 # load env from vault in prod
 if [ "$NODE_ENV" = "production" ]; then
 #  source /vault/secrets/env
+  yarn permissions:seed
   exec yarn ts-node src/app.ts
 #  exec yarn ts-node src/app.ts
 fi
@@ -11,6 +12,7 @@ fi
 # load env from .env
 if [ "$NODE_ENV" != "production" ]; then
   source .env
+  yarn permissions:seed
   # prettify pino logs in development
   exec yarn nodemon src/app.ts #| ./node_modules/.bin/pino-pretty
 fi
