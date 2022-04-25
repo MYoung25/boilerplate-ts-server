24d64f9b6a05bd6ce16149b5108d76ccf615b852
Michael Young <michael25young25@gmail.com>
2022-04-25 17:50:47 -0400

    production performance tweak with ts-node

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 if [ "$NODE_ENV" = "production" ]; then
 #  source /vault/secrets/env
   yarn permissions:seed
-  exec yarn ts-node src/app.ts
+  exec yarn ts-node --transpile-only src/app.ts
 #  exec yarn ts-node src/app.ts
 fi
 
