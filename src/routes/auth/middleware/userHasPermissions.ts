2d870a6f8c3e2dc6e1c8d29e0ad8824659499edf
Michael Young <michael25young25@gmail.com>
2022-04-25 09:18:34 -0400

    add comments

--- src/routes/auth/middleware/userHasPermissions.ts
+++ src/routes/auth/middleware/userHasPermissions.ts
@@ -5,19 +5,24 @@ import { publicRoute } from "./publicRoute"
 function permissionsCheck (req: Request, res: Response, next: NextFunction) {
     // exit if the request is not from a logged-in user
     if (!req.isAuthenticated()) return res.sendStatus(401)
+
+    // parse request to create permissions string
     const baseUrl = req.baseUrl.slice(1).toLowerCase()
     const path = req.route.path.slice(1).replace(':', '').toLowerCase()
     const method = req.method.toLowerCase()
 
+    // create permissions string
     const permission = `${baseUrl}.${path ? path + '.' : ''}${method}`
 
     const user = (req.user as SerializedUser)
     if (user.role && 'permissions' in user.role) {
+        // see if user has permission string in their hashPermissions
         const hasPermission = user.hashPermissions[permission]
         if (hasPermission) {
             return next()
         }
     }
+    // user does not have permissions, send a 401 Unauthorized error
     res.sendStatus(401)
 }
 
