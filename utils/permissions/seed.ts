f861fd22b322e98f506db70f0ed70829cfa0d344
Michael Young <michael25young25@gmail.com>
2022-04-25 14:48:05 -0400

    delete permissions that are no longer needed when permissions:seed is called on startup

--- utils/permissions/seed.ts
+++ utils/permissions/seed.ts
@@ -45,12 +45,25 @@ fs.readdir('./src/routes/', async (err, files) => {
         process.exit(1)
     }
 
+    // TODO delete permissions that no longer exist
+    const allCurrentPermissions = await Permissions.find({})
+    const hashedCurrentPermissions = allCurrentPermissions.reduce((acc: Record<string, IPermissions>, curr) => {
+        acc[curr.name] = curr
+        return acc
+    }, {})
+
     const savedPermissions = await Promise.all(permissionsList.map(async (permission: HydratedDocument<IPermissions>) => {
         const { name, group } = permission
+        if (hashedCurrentPermissions[name]) {
+            // remove from current permissions, anything left at the end will be deleted
+            delete hashedCurrentPermissions[name]
+        }
         return Permissions.findOneAndUpdate({ name: permission.name }, {
             name, group
         }, { upsert: true, returnDocument: 'after' })
     }))
+
+    await Permissions.deleteMany({ _id: { $in: Object.values(hashedCurrentPermissions).map(({ _id }) => _id) } })
 
     await Roles.findOneAndUpdate(
         { name: 'SUPERADMIN' },
