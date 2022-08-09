f65fea61896d92fe9da40661dbb1e75116cfe72c
Michael Young <michael25young25@gmail.com>
2022-08-08 23:49:59 -0400

    update from IS site

--- src/routes/auth/Google/index.ts
+++ src/routes/auth/Google/index.ts
@@ -6,19 +6,20 @@ import handleOauthCallback from "./handleOauthCallback"
 
 export const router = Router()
 
-passport.use(new Strategy(
-    {
-        clientID: config.google_oauth.client_id,
-        clientSecret: config.google_oauth.client_secret,
-        callbackURL: "/auth/google/callback",
-    },
-    handleOauthCallback
-))
-
-router.get('/', passport.authenticate('google', { scope: ['profile', 'email'] }))
-
 /* istanbul ignore next */
-router.get('/callback', passport.authenticate('google'), (req, res) => {
+router.get('/callback',
+    (req, res, next) => {
+        const googleStrategy = new Strategy(
+            {
+                clientID: config.google_oauth.client_id,
+                clientSecret: config.google_oauth.client_secret,
+                callbackURL: `${req.headers.origin}/auth/google/callback`,
+            },
+            handleOauthCallback
+        )
+        passport.authenticate(googleStrategy)(req, res, next)
+    },
+    (req, res) => {
     res.sendStatus(204)
 })
 
